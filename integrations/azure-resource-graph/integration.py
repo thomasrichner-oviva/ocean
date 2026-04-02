5b72e54e198594344ebd05b1dbc9cc840a186cfe
Melody Anyaegbulam <melody.anyaegbulam@port.io>
2026-04-02 19:28:10 +0100

    Fix Azure resource graph import issue

--- integrations/azure-resource-graph/integration.py
+++ integrations/azure-resource-graph/integration.py
@@ -12,13 +12,14 @@ from pydantic import Field, BaseModel
 
 class AzureAPIParams(BaseModel):
     version: str = Field(
-        ...,
+        title="Version",
         description="The API version to use for the resource graph",
     )
 
 
 class AzureSubscriptionParams(BaseModel):
     api_params: AzureAPIParams = Field(
+        title="API Params",
         default=AzureAPIParams(version="2022-12-01"),
         description="The API parameters to use for the subscription",
         alias="apiParams",
@@ -27,24 +28,29 @@ class AzureSubscriptionParams(BaseModel):
 
 class AzureResourceGraphSelector(Selector):
     graph_query: str = Field(
-        ...,
+        title="Graph Query",
         alias="graphQuery",
-        description="The Resource Graph query to use for the resource graph",
+        description="Use this to map Azure Resource Graph data by setting the table name to a supported <a target='_blank' href='https://learn.microsoft.com/en-us/azure/governance/resource-graph/reference/supported-tables-resources'>Azure Resource Graph table</a>",
     )
     api_params: AzureAPIParams = Field(
+        title="API Params",
         default=AzureAPIParams(version="2024-04-01"),
         description="The API parameters to use for the resource graph",
         alias="apiParams",
     )
     subscription: AzureSubscriptionParams = Field(
+        title="Subscription",
         default=AzureSubscriptionParams(),
         description="The subscription to use for the resource graph",
         alias="subscription",
     )
 
 
-class AzureResourceConfig(ResourceConfig):
-    selector: AzureResourceGraphSelector
+class AzureResourceGraphConfig(ResourceConfig):
+    selector: AzureResourceGraphSelector = Field(
+        title="Azure Resource Selector",
+        description="Selects Azure resources to query via the Resource Graph",
+    )
     kind: Literal["resource"] = Field(
         title="Azure Resource",
         description="An Azure resource queried via the Resource Graph",
@@ -52,7 +58,10 @@ class AzureResourceConfig(ResourceConfig):
 
 
 class AzureResourceContainerConfig(ResourceConfig):
-    selector: AzureResourceGraphSelector
+    selector: AzureResourceGraphSelector = Field(
+        title="Azure Resource Container Selector",
+        description="Selects Azure resource containers to query via the Resource Graph",
+    )
     kind: Literal["resourceContainer"] = Field(
         title="Azure Resource Container",
         description="An Azure resource container (e.g. subscription, resource group) queried via the Resource Graph",
@@ -63,7 +72,10 @@ class AzureSubscriptionSelector(Selector, AzureSubscriptionParams): ...
 
 
 class AzureSubscriptionResourceConfig(ResourceConfig):
-    selector: AzureSubscriptionSelector
+    selector: AzureSubscriptionSelector = Field(
+        title="Azure Subscription Selector",
+        description="Selects Azure subscriptions to query via the Resource Graph",
+    )
     kind: Literal["subscription"] = Field(
         title="Azure Subscription",
         description="An Azure subscription",
@@ -72,7 +84,7 @@ class AzureSubscriptionResourceConfig(ResourceConfig):
 
 class AzurePortAppConfig(PortAppConfig):
     resources: list[
-        AzureResourceConfig
+        AzureResourceGraphConfig
         | AzureResourceContainerConfig
         | AzureSubscriptionResourceConfig
     ] = Field(default_factory=list)
